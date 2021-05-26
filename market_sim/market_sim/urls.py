7901135eb4b073d7c47b8f1dd0086e23641b0083
KristianMSchmidt <krms@kvuc.dk>
2021-05-26 14:33:21 +0200

    uninstall of django debug toolbar

--- market_sim/market_sim/urls.py
+++ market_sim/market_sim/urls.py
@@ -21,9 +21,3 @@ urlpatterns = [
     path('', include('market.urls')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
