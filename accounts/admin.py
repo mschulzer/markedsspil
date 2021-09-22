122402260b7e41ad084dddff1e4b64bb5cd93489
KristianMSchmidt <kmollerschmidt@gmail.com>
2021-09-22 15:15:12 +0200

    modified accounts.admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,7 +7,6 @@ CustomUser = get_user_model()
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'username']
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
